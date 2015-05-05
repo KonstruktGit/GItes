d17cc8d1fd8806ef26ab03ad9dd8369715990550
wizkidweb16 <wizkidweb16@gmail.com>
2015-05-05 16:51:03 -0400

    Changing node server structure

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 var express = require('express'),
 	handlebars = require('express-handlebars'),
-	app = express(),
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	logger = require('morgan'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser');
+
+var routes = require('./routes/index'),
+	users = require('./routes/users');
+
+var	app = express(),
 	port = 3000;
 
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', handlebars({
 	defaultLayout: 'main',
 }));
 app.set('view engine', 'handlebars');
 
-app.use('/app', express.static(__dirname + '/app'));
+// uncomment after placing favicon in /public
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-	res.render('home');
+app.use('/', routes);
+app.use('/users', users);
+
+/* Catch 404 and forwarding to error handler */
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-app.listen(port, function() {
-	console.log("Listening on localhost:"+port+" ...");
+// dev error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
+}
+
+// production error handler
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
+
+module.exports = app;
